bc6720fcf93344e663f28d0b936c8bfddedff6e4
pelusa-v <razor0164@gmail.com>
2024-09-08 22:43:13 -0500

    Practicing Azure Cosmos stored procedures

--- azure-cosmos/crud-practice/Program.cs
+++ azure-cosmos/crud-practice/Program.cs
@@ -1,8 +1,8 @@
 ﻿using crud_practice;
 using crud_practice.Models;
 
-var uri = "https://practicecosmos1.documents.azure.com:443/";
-var key = "HrMWoJ7e9fUp3bpK7QUNedTGVQF2HagA1uXFMDiFywYGtL2KDBxIkp2Tmu1fy0PuY7WrBbNZBmI0ACDbMu7E9Q==";
+var uri = "";
+var key = "";
 var cosmos = new AzureCosmosUtils(uri, key);
 
 // await cosmos.CreateDatabase("crud_practice1");
