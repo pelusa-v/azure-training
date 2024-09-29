ab87d54b288cf4a8cdc7cedbd2b1cc4ca220c987
pelusa-v <razor0164@gmail.com>
2024-09-29 02:05:41 -0500

    Studying azure microsoft entra ID

---
+++ azure-entraid/msal-practice/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Identity.Client;
+
+namespace msal_practice;
+
+class Program
+{
+    private const string _clientId = "";
+    private const string _tenantId = "";
+    public static async Task Main(string[] args)
+    {
+        var app = PublicClientApplicationBuilder
+            .Create(_clientId)
+            .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+            .WithRedirectUri("http://localhost")
+            .Build();
+        string[] scopes = { "user.read" };
+        AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+        Console.WriteLine($"Token:\t{result.AccessToken}");
+    }
+}
